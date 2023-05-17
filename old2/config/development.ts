6316fe37aa436bfc606ab1f51d63235e36d09da3
maruyu998 <maruyu998@gmail.com>
2023-05-18 02:22:56 +0900

    git rm --cached old2

--- old2/config/development.ts
+++
@@ -1,8 +0,0 @@
-import * as secret from './secret';
-
-export const mongo_path = "mongodb://host.docker.internal:27017/moauth";
-export const session_secret = "gP75gbcgWgj.5%qSuE2rYv83S6-R~8$R#4HCJ_,C";
-export const hash_salt = "7Vh2.imC58KK6bq,/XL)Hk~B,u9euJLt&yCPBfnL";
-export const moauth_server = secret.moauth_server;
-export const app_id = "moauth";
-export const app_secret = secret.app_secret;
